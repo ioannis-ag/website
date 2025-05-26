6c27335b2155cc9e31928b5527586dc8e9bb50fa
ioannis-ag <sloths-cowherd0k@icloud.com>
2025-05-26 14:19:42 +0300

    Update server.js

--- server.js
+++ server.js
@@ -29,4 +29,5 @@ db.prepare(`
 const routes = require('./routes/index');
 app.use('/', routes);
 
-app.listen(3000, () => console.log('Server running on port 3000'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
